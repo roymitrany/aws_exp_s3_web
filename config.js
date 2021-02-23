ee7506a29cac1068023007e9e68f6b97416c12fc
Roy Mitrany <31730910+roymitrany@users.noreply.github.com>
2021-02-23 15:53:41 +0200

    Update config.js
    set urls

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-samplePresenceUrl="URL for Sample Presence API"
-AttendanceUrl="URL for Getting attendance API"
+samplePresenceUrl="https://4rg7hkyq4c.execute-api.us-east-1.amazonaws.com/default/GwToBrowser"
+AttendanceUrl="https://4rg7hkyq4c.execute-api.us-east-1.amazonaws.com/default/DbReader"
